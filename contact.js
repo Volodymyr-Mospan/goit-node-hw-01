da11c8fcd8055fe62d1150d639018b815ba29d74
Volodymyr <mospan.volodymyr@gmail.com>
2023-05-03 15:49:58 +0300

    adds the function of deleting contact by id

--- contact.js
+++ contact.js
@@ -1,29 +1,36 @@
-const fs = require("fs/promises");
-const path = require("path");
+const fs = require('fs/promises');
+const path = require('path');
 
-const contactsPath = path.join(__dirname, "./db/contacts.json");
+const contactsPath = path.join(__dirname, './db/contacts.json');
 
-// TODO: задокументувати кожну функцію
 async function listContacts() {
-  const listContacts = await fs.readFile(contactsPath);
-  return JSON.parse(listContacts);
+    const listContacts = await fs.readFile(contactsPath);
+    return JSON.parse(listContacts);
 }
 
 async function getContactById(contactId) {
-  const allContact = await listContacts();
-  const contact = allContact.find((contact) => contact.id === contactId);
-  return contact;
+    const allContact = await listContacts();
+    const contact = allContact.find(contact => contact.id === contactId);
+    return contact || null;
 }
 
-function removeContact(contactId) {
-  // ...твій код
+async function removeContact(contactId) {
+    const allContact = await listContacts();
+    const contactIndex = allContact.findIndex(contact => contact.id === contactId);
+    if (contactIndex === -1) {
+        return null;
+    }
+    const contact = allContact.splice(contactIndex, 1);
+    await fs.writeFile(contactsPath, JSON.stringify(allContact, null, 2));
+    return contact;
 }
 
 function addContact(name, email, phone) {
-  // ...твій код
+    // ...твій код
 }
 
 module.exports = {
-  listContacts,
-  getContactById,
+    listContacts,
+    getContactById,
+    removeContact,
 };
--- index.js
+++ index.js
@@ -1,15 +1,26 @@
-const contacts = require("./contact");
+const contacts = require('./contact');
 
 const invokeAction = async ({ action, id, name, email, phone }) => {
-  switch (action) {
-    case "listContacts":
-      const allContacts = await contacts.listContacts();
-      return console.log(allContacts);
-    case "getContactById":
-      const contact = await contacts.getContactById(id);
-      return console.log(contact);
-  }
+    switch (action) {
+        case 'listContacts':
+            const allContacts = await contacts.listContacts();
+            return console.log(allContacts);
+        case 'getContactById':
+            const contact = await contacts.getContactById(id);
+            return console.log(contact);
+        case 'removeContact':
+            const deletedContact = await contacts.removeContact(id);
+            return console.log(deletedContact);
+    }
 };
 
-// console.log(contacts.listContacts());
-invokeAction({ action: "getContactById", id: "1" });
+// invokeAction({ action: "listContacts"});
+// invokeAction({ action: 'getContactById', id: '1' });
+// invokeAction({ action: 'removeContact', id: '1' });
+
+//   {
+//     "id": "1",
+//     "name": "Volodymyr Mospan",
+//     "email": "MospanVolodymyr@gmail.com",
+//     "phone": "(063) 030-7592"
+//   }
