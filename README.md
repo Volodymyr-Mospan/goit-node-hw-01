6831449bbb466a38d9be3c2de04d67c45cd98974
Volodymyr <mospan.volodymyr@gmail.com>
2023-05-06 16:32:28 +0300

    adds console comands on commander

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # goit-node-hw-01
+
+Це домашня робота №1 з Node.js. Проєкт представляє собою консольний додаток лдя зчитування,
+створення та видалення контактів із файло контактів.
+
+## Консольні команди
+
+--action list -виводить таблицю контактів, [--action list](https://i.ibb.co/XpZtnMr/list.png)
+
+--action get --id 'user id' -виводить необхідний контакт,
+[--action get](https://i.ibb.co/F8Mt3hC/get.png)
+
+--action add --name 'user name' --email 'user email' --phone 'user phone' -виводить необхідний
+контакт, [--action add](https://i.ibb.co/F8Mt3hC/add.png)
+
+--action remove --id 'user id' -видаляє необхідний контакт,
+[--action remove](https://i.ibb.co/F8Mt3hC/remove.png)
+
+--action help -виводить список всіх екшенів, [--action help](https://i.ibb.co/t4B2chW/help.png)
--- index.js
+++ index.js
@@ -1,44 +1,42 @@
-const argv = require('yargs').argv;
+const { Command } = require('commander');
+// const argv = require('yargs').argv;
 const contacts = require('./contact');
+
+const program = new Command();
+program
+    .option('-a, --action <type>', 'choose action')
+    .option('-i, --id <type>', 'user id')
+    .option('-n, --name <type>', 'user name')
+    .option('-e, --email <type>', 'user email')
+    .option('-p, --phone <type>', 'user phone');
+
+program.parse(process.argv);
+
+const argv = program.opts();
 
 const invokeAction = async ({ action, id, name, email, phone }) => {
     switch (action) {
-        case 'listContacts':
+        case 'list':
             const allContacts = await contacts.listContacts();
-            return console.log(allContacts);
-        case 'getContactById':
+            return console.table(allContacts);
+        case 'get':
             const contact = await contacts.getContactById(id);
             return console.log(contact);
-        case 'removeContact':
+        case 'remove':
             const deletedContact = await contacts.removeContact(id);
             return console.log(deletedContact);
-        case 'addContact':
+        case 'add':
             const addedContact = await contacts.addContact(name, email, phone);
             return console.log(addedContact);
         case 'help':
             return console.log(
-                'listContacts - to see all contacts \ngetContactById - to see contact \nremoveContact - to remove contact \nremoveContact - to add contact \nhelp - to see all actions'
+                'list - to see all contacts \nget - to see contact \nremove - to remove contact \nadd - to add contact \nhelp - to see all actions'
             );
         default:
-            return console.log('Unknow action! Input "-- action help" to see all actions');
+            return console.warn(
+                '\x1B[31m Unknow action! Input "-- action help" to see all actions'
+            );
     }
 };
 
 invokeAction(argv);
-
-// invokeAction({ action: "listContacts"});
-// invokeAction({ action: 'getContactById', id: '1' });
-// invokeAction({ action: 'removeContact', id: '_AXwvI6gtJ2fliGWr3Tiz' });
-// invokeAction({
-//     action: 'addContact',
-//     name: 'Volodymyr',
-//     email: 'mospan.volodymyr@gmail.com',
-//     phone: '(063) 030-7592',
-// });
-
-//   {
-//     "id": "1",
-//     "name": "Volodymyr Mospan",
-//     "email": "MospanVolodymyr@gmail.com",
-//     "phone": "(063) 030-7592"
-//   }
